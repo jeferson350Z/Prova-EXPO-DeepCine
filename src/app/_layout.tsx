30d9a2283577aaa251e5b2eea1c435570a6ad3b9
jeferson350Z <jefersonjd045@gmail.com>
2026-09-20 13:32:29 -0300

    feito: configurar Stack no layoutraiz

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { cores } from "../theme/cores";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: cores.fundo },
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="busca" />
+        <Stack.Screen name="detalhes" />
+      </Stack>
+    </>
+  );
 }
